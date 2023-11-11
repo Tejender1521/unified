2cb808f9f13dd92b2dba6a51c7c96c15e4c7aa66
Tejender Upadhyay <2020kucp1019@iiitkota.ac.in>
2023-11-11 20:47:38 +0530

    Git host updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# gym-typescript
+# Unified2024
 
 
--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-10 basis-1/2 md:mt-0">
           <img alt="logo" src={Logo} className="h-24 rounded-lg" />
-          <p className="my-5">
+          {/* <p className="my-5">
             Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
             purus et arcu massa dictum condimentum. Augue scelerisque iaculis
             orci ut habitant laoreet. Iaculis tristique.
-          </p>
+          </p> */}
           <p>Copyright © 2024 UNIfied 2024 - All Rights Reserved.</p>
         </div>
         {/* <div className="mt-16 basis-1/4 md:mt-0">
